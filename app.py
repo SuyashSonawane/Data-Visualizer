cabfda0b529c7b4a45099f7a2442881536340c3b
suyashsonawane <suyashsonawane007@gmail.com>
2021-09-18 14:33:51 +0530

    adding dataset

--- app.py
+++ app.py
@@ -17,30 +17,6 @@ from sklearn.metrics import confusion_matrix
 import statistics
 
 
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
-
-
-set_png_as_page_bg('mainbg.jpg')
-
-
 st.title('Mushroom Dataset Analysis')
 st.sidebar.title('Mushroom Dataset Analysis')
 
